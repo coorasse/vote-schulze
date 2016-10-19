65a1497b662ef00c979c6f1619477f1557160d2a
Alessandro Rodi <alessandro.rodi@renuo.ch>
2016-10-19 20:48:38 +0200

    move requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
+
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'schulze_vote'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
 RSpec.configure do |_config|
 end
